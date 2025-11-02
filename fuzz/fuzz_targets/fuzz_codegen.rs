958acbc637d404d233d12cd57c42c8c9abb49f93
nynrathod <64688014+nynrathod@users.noreply.github.com>
2025-11-02 12:16:21 +0530

    added fuzz targets for fuzzing implementation

---
+++ fuzz/fuzz_targets/fuzz_codegen.rs
@@ -0,0 +1,39 @@
+#![no_main]
+use bumpalo::Bump;
+use doo::analyzer::SemanticAnalyzer;
+use doo::lexar::lexer::lex;
+use doo::mir::builder::MirBuilder;
+use doo::parser::ast::AstNode;
+use doo::parser::Parser;
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if data.is_empty() {
+        return;
+    }
+
+    if data.len() > 1024 {
+        return;
+    }
+
+    if let Ok(s) = std::str::from_utf8(data) {
+        let arena = Bump::new();
+        let tokens = lex(s, &arena);
+
+        if tokens.len() > 5000 {
+            return;
+        }
+
+        let mut parser = Parser::new(&tokens);
+        if let Ok(mut ast) = parser.parse_program() {
+            if let AstNode::Program(ref mut nodes) = ast {
+                let mut analyzer = SemanticAnalyzer::new(None);
+                if analyzer.analyze_program(nodes).is_ok() {
+                    let mut mir_builder = MirBuilder::new();
+                    mir_builder.build_program(nodes);
+                    mir_builder.finalize();
+                }
+            }
+        }
+    }
+});
